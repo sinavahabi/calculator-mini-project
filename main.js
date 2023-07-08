549de5440c05f032fdc89b19c0980196ce27f015
sinavahabi <sinavahabiz8@gmail.com>
2023-07-08 18:50:16 +0330

    updating codes

--- main.js
+++ main.js
@@ -10,26 +10,54 @@
 // Defining "power" variable to check some conditions.
 let power = false;
 
+// Accessing to DOM objects.
+const btnElemList = document.querySelectorAll(".buttons");
+const inputElemList = document.getElementsByTagName("input");
+
+// Using loops to modify style properties values of button elements when they get focused in.
+for (let input=0; input < inputElemList.length; input++) {
+    inputElemList[input].addEventListener("focusin", () => {
+        inputElemList[input].style.opacity = ".6";
+    });
+}
+
+// Using loops to modify style properties values of button elements when they get focused out.
+for (let input=0; input < inputElemList.length; input++) {
+    inputElemList[input].addEventListener("focusout", () => {
+        inputElemList[input].style.opacity = "1";
+    });
+}
+
 // Defining "add" function to show clicked buttons results on calculator display.
-function add(val){        
+function add(val) {        
     document.getElementById("display").value += val.target.value;
     // If "power" value is true ('^' button has been clicked) we perform following operation.
-    if(power) {
+    if (power) {
         let powNumbers = document.getElementById("display").value.split('^');
         // Check if user didn't enter any number first, and asked for exponentiation operation.
-        if(powNumbers[0] == "") {
-            alert("You have to add a number first before you want to perform exponentiation operation!");
-            document.getElementById("display").value = '';
+        if (powNumbers[0] == "") {
+            // If user wrong actions are detected, following modifications will occur and user will face some changes on the calculator appearance and button actions.
+            for (let btn=0; btn < btnElemList.length; btn++) {
+                btnElemList[btn].disabled = true;
+                btnElemList[btn].style.cursor = "not-allowed";
+                btnElemList[btn].style.opacity = ".5";
+            }
+            document.getElementById("display").value = 'Invalid Input!';
             power = false;
         } else {
             // Check if both entered values (first and second numbers) are actually numbers and valid (true) for exponentiation operation!
-            if(Number(powNumbers[0]) && Number(powNumbers[1])) {
+            if (Number(powNumbers[0]) && Number(powNumbers[1])) {
                 document.getElementById("display").value = powNumbers[0] ** powNumbers[1];
                 power = false;
             } else {
+                // If user wrong actions are detected, following modifications will occur and user will face some changes on the calculator appearance and button actions.
                 power = false;
-                document.getElementById("display").value = '';
-                alert("Invalid input!");   
+                for (let btn=0; btn < btnElemList.length; btn++) {
+                    btnElemList[btn].disabled = true;
+                    btnElemList[btn].style.cursor = "not-allowed";
+                    btnElemList[btn].style.opacity = ".5";
+                } 
+                document.getElementById("display").value = 'Invalid Input!';
             }
         }
     }
@@ -37,18 +65,36 @@ function add(val){
 
 // By defining this function we'll complete most of our calculations here which happens when user click on "=" button.
 function calculation() {
-        // Check if user didn't ask for calculation when display value was empty.
-        if(eval(document.getElementById("display").value)) {
-            document.getElementById("display").value = eval(document.getElementById("display").value);
-        } else {
-            // Handling "undefined" error output on calculator display somehow!
-            document.getElementById("display").value = 0; 
+        // Check if user didn't ask for weird and invalid calculation.
+        try {
+            // Check if user didn't ask for calculation when display value was empty.
+            if (eval(document.getElementById("display").value)) {
+                document.getElementById("display").value = eval(document.getElementById("display").value);
+            } else {
+                // Handling "undefined" error output on calculator display somehow!
+                document.getElementById("display").value = "0"; 
+            }
         }
+        // Handling errors by "try catch(e)" conditions!
+        catch(err) {
+            // If user wrong actions are detected, following modifications will occur and user will face some changes on the calculator appearance and button actions.
+            for (let btn=0; btn < btnElemList.length; btn++) {
+                btnElemList[btn].disabled = true;
+                btnElemList[btn].style.cursor = "not-allowed";
+                btnElemList[btn].style.opacity = ".5";
+            }
+            document.getElementById("display").value = "Invalid Input!";
+        }    
 }
 
 // Defining clear or reset functionality button in order to make calculators display empty.
 function reset() {
     document.getElementById("display").value = '';
+    for (let btn=0; btn < btnElemList.length; btn++) {
+        btnElemList[btn].disabled = false;
+        btnElemList[btn].style.cursor = "pointer";
+        btnElemList[btn].style.opacity = "1";
+    }
 }
 
 // Defining backspace or delete functionality button in order to minus last displayed result number one by one for each press at a time. 
@@ -62,27 +108,32 @@ function mathCalculation(mathFunc) {
     if (mathFunc == 'pow') {
         power = true;
         document.getElementById("display").value += '^';
-    } else{
+    } else {
         let val = document.getElementById("display").value;
         // Check if user is asking for mathematical result when nothing is entered yet, and calculators result display is empty!
-        if(Math[mathFunc](val) == "") {
+        if (Math[mathFunc](val) == "") {
             document.getElementById("display").value = 0; 
         } else {
             // Check if user asked for wrong mathematical operations or pressing irrelevant buttons Continuously.
-            if(Math[mathFunc](val)){
+            if (Math[mathFunc](val)){
                 document.getElementById("display").value = Math[mathFunc](val);
             } else {
-                document.getElementById("display").value = '';
-                // Handling "NaN" error output on calculators display somehow!
-                alert("Invalid input!");
+                // Handling "NaN" output result on calculators display somehow!
+                document.getElementById("display").value = 'Invalid Input!';
+                // If user wrong actions are detected, following modifications will occur and user will face some changes on the calculator appearance and button actions.
+                for (let btn=0; btn < btnElemList.length; btn++) {
+                    btnElemList[btn].disabled = true;
+                    btnElemList[btn].style.cursor = "not-allowed";
+                    btnElemList[btn].style.opacity = ".5";
+                }
             }            
         }
     }
 }
 
 // Adding digital clock feature to program.
-
-function clock(){
+function clock() {
+    // Getting current live date, hour, minute and etc.
     let date = new Date();
     let hour = date.getHours();
     let minute = date.getMinutes();
@@ -90,28 +141,31 @@ function clock(){
     let time = "AM";
     let dateString = String(date);
 
-    if(hour > 12) {
+    // Using conditions to show different appearance.
+    if (hour > 12) {
         hour -= 12;
         time = "PM";
     }
 
-    if(hour < 10) {
+    if (hour < 10) {
         hour = "0" + hour;
     }
 
-    if(minute < 10) {
+    if (minute < 10) {
         minute = "0" + minute;
     }
 
-    if(second < 10) {
+    if (second < 10) {
         second = "0" + second;
     }
 
+    // DOM searching.
     let timeDisplay = `${hour}:${minute}:${second} ${time}`;
     let dateDisplay = dateString.slice(0,15);
     document.getElementById("clock").textContent = timeDisplay;
     document.getElementById("date").textContent = dateDisplay;
 
+    // Creating nested setTimeout.
     setTimeout(clock, 1000);
 }
 
--- readme.txt
+++ readme.txt
@@ -1,12 +1,15 @@
 Web Application Calculator
 
-Features: Basic, HTML "onclick" event, Error handling, Nested “setTimeout”
+Features: Basic, HTML events, Error handling, Nested “setTimeout”, DOM Searching
 
 Languages: JavaScript, HTML, CSS
 
 ''' This is my very first programming with JavaScript, HTML and CSS, so these codes are very basic and primary.'''
-Here we have a basic web application calculator which has some Math methods such as round, ceil, floor, square root, absolute and exponentiation mathematical operators beside basic calculations which is done by “eval” function.
-In this program, some “undefined” and “NaN” errors are somehow handled by JavaScript alerts or setting the calculators display empty (0); that is all done by using basic conditional statements.
+Here we have a basic web application calculator which has some Math functions such as round, ceil, floor, square root, absolute and exponentiation mathematical operators beside basic calculations which is done by “eval” function.
+In this program, some “undefined” and “NaN” outputs are handled by JavaScript conditional operators like “try catch(e)” or simple if-else statement.
 There is also a digital clock feature which has been added by using nested setTimeout which gives the user exact momentary date and time.
+This mini project includes more error handling and proper responds to user when they commit wrong actions. For example, when they try to perform mathematical operation on a negative number, the program will disable all buttons except reset button ("C") and, replaces recent values on calculator display with (“Invalid Input”) message.
+So, user has to click on reset button ("C") in order to be able to continue using calculator. Also calculator appearance will look different when buttons are disabled. Buttons opacity will decrease and cursor will be set as "not-allowed" when user hovers the mouse on buttons.
+Note that After user clicks on reset ("C") button to continue calculating, everything will go back to normal and appearance will change accordingly.
 Note: This mini project was completely handled by git.
 Note: Codes documentation and descriptions are more obvious in comments among the codes on each file.
